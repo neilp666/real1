b75db2ab51235170a4faafc65277e601469c7dda
neil patel <neil@myindianweddingapp.com>
2015-08-25 12:27:41 +0100

    Updated db file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825092137) do
+ActiveRecord::Schema.define(version: 20150825104811) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150825092137) do
     t.string   "organisation"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
