54a29c1d91df1535df5e15e21e80ddf490339aef
neil patel <neil@myindianweddingapp.com>
2015-08-27 11:12:52 +0100

    Removed image attachments

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,17 +1,5 @@
 class Project < ActiveRecord::Base
 
   belongs_to :user
-  has_attached_file :image
-  has_attached_file :resource
-
-  validates_attachment_content_type :image, 
-  content_type:  /^image\/(png|gif|jpeg)/,
-  message: "Only images allowed"
-
-  validates_attachment_content_type :resource,
-  content_type: ['application/pdf'],
-  message: "Only pdfs allowed"
-
-  
-  
+ 
 end
